734b2927483bd491bfd7859193359b1570e9f309
Gregory Hunkeler <gregoryhunkeler@gmail.com>
2023-03-13 11:15:49 -0300

    access token function and cache store

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,3 +1,7 @@
+const NodeCache = require("node-cache");
+const cache = new NodeCache();
+const axios = require("axios");
+
 async function getToken(req, res) {
   const result = await fetch("https://accounts.spotify.com/api/token", {
     method: "POST",
@@ -5,15 +9,74 @@ async function getToken(req, res) {
       "Content-Type": "application/x-www-form-urlencoded",
       Authorization:
         "Basic " +
-        btoa(process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET),
+        btoa(
+          process.env.SPOTIFY_CLIENT_ID +
+            ":" +
+            process.env.SPOTIFY_CLIENT_SECRET
+        ),
     },
     body: "grant_type=client_credentials",
   });
 
   const data = await result.json();
+  console.log(data);
   res.status(200).json({ token: data.access_token });
 }
 
+async function authenticate() {
+  let clientId = process.env.SPOTIFY_CLIENT_ID;
+  let clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
+    },
+    body: "grant_type=client_credentials",
+  });
+
+  const data = await response.json();
+
+  return {
+    accessToken: await data.access_token,
+    expiresIn: await data.expires_in,
+  };
+}
+
+async function spotifyAccessToken() {
+  const currentToken = cache.get("currentSpotifyToken");
+
+  let returnValue = null;
+
+  if (!currentToken) {
+    const newToken = await authenticate();
+    cache.set("currentSpotifyToken", newToken.accessToken);
+    cache.ttl("currentSpotifyToken", newToken.expiresIn);
+
+    returnValue = newToken.accessToken;
+  } else {
+    returnValue = currentToken;
+  }
+
+  return returnValue;
+}
+
+async function getAlbums(req, res) {
+  const result = await axios({
+    method: "get",
+    baseUrl: `https://api.spotify.com/v1/albums/${req.params.id}`,
+    headers: {
+      Authorization: `Bearer ${await spotifyAccessToken()}`,
+    },
+  });
+  console.log(result);
+  // res.status(200).json(result);
+}
+
 module.exports = {
+  authenticate,
+  spotifyAccessToken,
   getToken,
+  getAlbums,
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const routes = express.Router();
 const apiController = require("../controllers/apiController");
 
-routes.get("/token", apiController.getToken);
+routes.get("/token", apiController.spotifyAccessToken);
+routes.get("/album/:id", apiController.getAlbums);
 
 module.exports = routes;
