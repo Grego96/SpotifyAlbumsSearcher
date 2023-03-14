5876e56481cd5cfccec8beeae3f7237ea73e36b0
Gregory Hunkeler <gregoryhunkeler@gmail.com>
2023-03-14 15:49:47 -0300

    env example

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const port = process.env.APP_PORT || 8000;
 const dbInitalSetup = require("./dbInitialSetup");
 const cors = require("cors");
 
+app.set("trust proxy", true);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
